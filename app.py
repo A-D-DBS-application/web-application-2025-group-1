21d80caf571ae1d75c8e505d90ce74061df70dcc
JaronDec <jaron.decaluwe@ugent.be>
2025-11-02 16:57:26 +0100

    upd

--- app.py
+++ app.py
@@ -1,18 +1,5 @@
-from flask import Flask
+from app import create_app
+app = create_app()
 
-app = Flask(__name__)
-
-@app.route('/')
-def html():
-    return '<h1>stoel</h1>'
-
-@app.route('/s')
-def index():
-    return 'stoel'
-
-@app.route('/multiply/<int:n1>/<int:n2>')
-def multiply(n1, n2):
-    return f'{n1} * {n2} = {n1*n2}'
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
