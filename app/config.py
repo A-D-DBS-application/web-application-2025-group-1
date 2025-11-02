8ca3d6a3616e275076eafa0d2e050bcdbb23afe9
vanbesienaxelle <axelle.vanbesien@ugent.be>
2025-11-02 15:53:50 +0100

    connecten database

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Group1_ADDBS@db.eoysewmdlgotspzgbpkb.supabase.co:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Group1_ADDBS!@db.eoysewmdlgotspzgbpkb.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
