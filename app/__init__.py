8b0b9bd2eef49a24a4698c0485de30ebccb15bcf
vanbesienaxelle <axelle.vanbesien@ugent.be>
2025-11-02 13:50:25 +0100

    Update __init__.py
    voor migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
 from flask import Flask
+from flask_migrate import Migrate
 from .models import db
 from .config import Config
+
+migrate = Migrate()  # <— toevoegen
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
-
-    with app.app_context():
-        db.create_all()  # Create sql tables for our data models
+    migrate.init_app(app, db)  # <— verbinden van migratiesysteem
 
     from .routes import main
     app.register_blueprint(main)
