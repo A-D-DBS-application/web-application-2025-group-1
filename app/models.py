382798de0805d701942742d5c6c4c2c7526d58c9
JaronDec <jaron.decaluwe@ugent.be>
2025-11-02 17:11:05 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,14 +1,36 @@
-
-#dit is de code die in de github flaskintro hier stond. 
-
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    __tablename__ = 'User'
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    phone_number = db.Column(db.Numeric)
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+class Trip(db.Model):
+    __tablename__ = 'Trip'
+    trip_id = db.Column(db.Integer, primary_key=True)
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    number_of_travelers = db.Column(db.Integer)
+    preferences = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.user_id'))
+
+    def __repr__(self):
+        return f'<Trip {self.trip_id}>'
+    
+class Traveller(db.Model):
+    __tablename__ = 'Travellers'
+    traveller_id = db.Column(db.Integer, primary_key=True)
+    age = db.Column(db.Date)
+    fitness = db.Column(db.String) 
+    trip_id = db.Column(db.Integer, db.ForeignKey('Trip.trip_id'))
+
+    def __repr__(self):
+        return f'<Traveller {self.traveller_id}>'
