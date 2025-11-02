a7b0980935d3cdeb0a072ea8542620695f980c1a
vanbesienaxelle <axelle.vanbesien@ugent.be>
2025-11-02 13:55:30 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
-from flask_migrate import Migrate
+from flask_migrate import Migrate # 1 importeren van flask-migrate
 from .models import db
 from .config import Config
 
-migrate = Migrate()  # <— toevoegen
+migrate = Migrate()  # 2 migratie-object aanmaken
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
-    migrate.init_app(app, db)  # <— verbinden van migratiesysteem
+    migrate.init_app(app, db)  # <— 3 verbinden van migratiesysteem koppel flask-migrate aan app en db
 
     from .routes import main
     app.register_blueprint(main)
